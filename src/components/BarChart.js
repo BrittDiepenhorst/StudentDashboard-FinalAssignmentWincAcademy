e45c1debdbe7957e33c9e33d64897bca1065a341
Britt Diepenhorst <brittdiepenhorst@hotmail.com>
2022-07-08 15:27:38 +0200

    commit

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,49 +1,135 @@
 import React from 'react';
-import MOCK_DATA from '../utils/MOCK_DATA.json'
-import mockdata_Winc from '../utils/mockdata_Winc.json'
+// import MOCK_DATA from '../utils/MOCK_DATA.json'
+// import mockdata_Winc from '../utils/mockdata_Winc.json'
 // import * as V from 'victory';
-import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
-
-const array1 = JSON.parse(MOCK_DATA)
-const array2 = JSON.parse(mockdata_Winc)
-const array3 = [{ ...array1, array2 }];
-
-console.log(array3);
+import {
+    VictoryBar,
+    VictoryChart,
+    VictoryAxis
+} from 'victory';
 
 function BarChart() {
-    const data = [
-        { exercise: 1, score: 1 },
-        { exercise: 2, score: 2 },
-        { exercise: 3, score: 3 },
-        { exercise: 4, score: 4 },
-        { exercise: 4, score: 5 },
-        { exercise: 2, score: 4 },
+
+    const data = [];
+
+    const getRandomRating = () => Math.random() * 5;
+    // const getRandomRating = () => 2;
+
+    let assignmentRatingAverage = [
+        { assignment: "W1D1-2" },
+        { assignment: "W1D1-1" },
+        { assignment: "W1D1-3" },
+        { assignment: "W1D2-1" },
+        { assignment: "W1D2-2" },
+        { assignment: "W1D3-1" },
+        { assignment: "W1D3-1" },
+        { assignment: "W1D3-2" },
+        { assignment: "W1D3-3" },
+        { assignment: "W1D3-4" },
+        { assignment: "W1D3-5" },
+        { assignment: "W1D3-6" }
     ];
+
+    assignmentRatingAverage = assignmentRatingAverage.map(avg => ({
+        assignment: avg.assignment,
+        difficultyRating: getRandomRating(),
+        enjoymentRating: getRandomRating()
+    }));
+
+    // Add label
+    const assignmentRatingAverageWithLabels = assignmentRatingAverage.map(avg => ({
+        assignment: avg.assignment,
+        difficultyRating: avg.difficultyRating,
+        enjoymentRating: avg.enjoymentRating,
+        label: `Opdracht ${
+            avg.assignment
+            }, difficultyRating: ${avg.difficultyRating.toFixed(
+                1
+            )}, enjoymentRating: ${avg.enjoymentRating.toFixed(1)}`
+    }));
 
     return (
         <div>
             <p>hello barchart</p>
 
-            <VictoryChart
-                // domainPadding will add space to each side of VictoryBar to
-                // prevent it from overlapping the axis
-                domainPadding={20}
-            >
+            <VictoryChart domainPadding={15}>
                 <VictoryAxis
                     // tickValues specifies both the number of ticks and where
                     // they are placed on the axis
-                    tickValues={[1, 2, 3, 4]}
-                    tickFormat={["Exercise A", "Exercise B", "Exercise C", "Exercise D"]}
+                    tickValues={[1, 2, 3, 4, 5]}
+                    tickFormat={[
+                        "SCRUM",
+                        "W1D1-1",
+                        "W1D2-1",
+                        "W1D2-2",
+                        "W1D2-3",
+                        "W1D2-4",
+                        "W1D2-5",
+                        "W1D3-1",
+                        "W1D3-2",
+                        "W1D3-4",
+                        "W1D3-5",
+                        "W1D3 - Project - Guess-the-number",
+                        "W1D4-1",
+                        "W1D4 - Project - Kleurentoggle",
+                        "W1D5 - Project - Galgje",
+                        "W2D1-1",
+                        "W2D1-2",
+                        "W2D2-1",
+                        "W2D2-2",
+                        "W2D2-3",
+                        "W2D3-1",
+                        "W2D3-2",
+                        "W2D3-3",
+                        "W2D4-1",
+                        "W2D4-2",
+                        "W2D4-3",
+                        "W2D5 - Project - Filmzoeker",
+                        "W3D1-1",
+                        "W3D1-2",
+                        "W3D1-3",
+                        "W3D1-4",
+                        "W3D2-1",
+                        "W3D2-2",
+                        "W3D2-3",
+                        "W3D3-1",
+                        "W3D3-2",
+                        "W3D3-3",
+                        "W3D3-4",
+                        "W3D4-1",
+                        "W3D4-2",
+                        "W3D5 - Project - Todo-List",
+                        "W4D2-1",
+                        "W4D2-2",
+                        "W4D2-3",
+                        "W4D2-4",
+                        "W4D3-1",
+                        "W4D3-2",
+                        "W4D3-3",
+                        "W4D3-4",
+                        "W4D3-5",
+                        "W4D3 - Project - Next-Level CSS",
+                        "W5D4-1",
+                        "W5D5 - Project - Lil_Playlist",
+                        "W6D1-1",
+                        "W6D2-1",
+                        "W6D2 - Project - Eindopdracht"
+                    ]}
                 />
+
                 <VictoryAxis
-                    dependentAxis
-                    // tickFormat specifies how ticks should be displayed
-                    tickFormat={(x) => (x)}
+                    // tickValues specifies both the number of ticks and where
+                    // they are placed on the axis
+                    tickValues={[1, 2, 3, 4, 5]}
+                    tickFormat={assignmentRatingAverageWithLabels.map(
+                        avg => avg.assignment
+                    )}
                 />
+                <VictoryAxis dependentAxis />
                 <VictoryBar
-                    data={data}
-                    x="exercise"
-                    y="score"
+                    data={assignmentRatingAverageWithLabels}
+                    x="assignment"
+                    y="enjoymentRating"
                 />
 
             </VictoryChart>
